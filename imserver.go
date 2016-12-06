fd6fd6210c8089ceaa22ef4c671030b540fa845b
shenqi329 <shenqi329@163.com>
2016-12-06 23:57:51 +0800

    grpc

--- imserver.go
+++ imserver.go
@@ -5,15 +5,39 @@ import (
 	controller "im/imserver/controller"
 	protocolBean "im/protocol/bean"
 	"log"
+	"net"
 	"runtime"
+
+	//"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	pb "im/grpc/message"
+	imserverGrpc "im/imserver/grpc"
 )
 
 func DeviceLogin(c imserver.Context) error {
 	return nil
 }
 
+func grpcRegister() {
+	lis, err := net.Listen("tcp", ":6005")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+
+	pb.RegisterMessageServer(s, &imserverGrpc.Message{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
 func main() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
+
+	grpcRegister()
 
 	var localUdpAddr string
 	if runtime.GOOS == "windows" {
---
+++ imserver/grpc/message.go
@@ -0,0 +1,15 @@
+package grpc
+
+import (
+	"golang.org/x/net/context"
+	//"google.golang.org/grpc"
+	"im/grpc/message"
+	"log"
+)
+
+type Message struct{}
+
+func (m *Message) CreateMessage(context.Context, *message.CreateMessageRequest) (*message.CreateMessageReply, error) {
+	log.Println("CreateMessage")
+	return nil, nil
+}
